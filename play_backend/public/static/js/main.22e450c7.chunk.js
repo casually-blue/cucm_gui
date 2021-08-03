26d1d06d7ddee54409e4159f613bf9b86b0c258e
Thomas Terry <darkforestsilence@gmail.com>
2021-08-03 15:40:43 -0500

    Remove old files

--- play_backend/public/static/js/main.22e450c7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),s=t(3),d=t.n(s),i=(t(8),t(9),t(0));var a=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("header",{className:"App-header",children:Object(i.jsx)("h1",{children:"CUCM Admin Page"})})})};d.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.22e450c7.chunk.js.map
