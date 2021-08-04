ba629cff43d9447c4cafda1b405f7e0d3f07071a
Thomas Terry <darkforestsilence@gmail.com>
2021-08-04 09:46:21 -0500

    Update npm app to use class instead of function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,38 @@
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>CUCM Admin Page</h1>
-          <a href="/phones">Get Phones JSon</a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            phones: null,
+            isLoaded: false,
+        };
+    }
+
+    componentDidMount() {
+        //fetch("http://localhost:9000/phones")
+        //    .then(res => res.json())
+        //    .then(
+        //        (result) => {
+        //            this.setState({
+        //                isLoaded: true,
+        //                phones: result
+        //            })
+        //        }
+        //    )
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>CUCM Admin Page</h1>
+                    <div className='phones-container'>
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
-
 export default App;
