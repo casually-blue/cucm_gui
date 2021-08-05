a68c8c41b457b266655eaf0c181dac5646a69a08
Thomas Terry <darkforestsilence@gmail.com>
2021-08-04 21:26:15 -0500

    Re-arrange

--- play_backend/app/org/coramdeoacademy/view/View.kt
+++ play_backend/app/org/coramdeoacademy/view/View.kt
@@ -3,14 +3,14 @@ package org.coramdeoacademy.view
 import org.coramdeoacademy.view.html.HTML
 import play.mvc.Result
 
-open class View(private val document: HTML) {
-    fun render(): Result {
-        return document.render()
-    }
-}
-
 fun html(init: HTML.() -> Unit): HTML {
     val html = HTML()
     html.init()
     return html
+}
+
+open class View(private val document: HTML) {
+    fun render(): Result {
+        return document.render()
+    }
 }
--- play_backend/app/org/coramdeoacademy/view/html/HTML.kt
+++ play_backend/app/org/coramdeoacademy/view/html/HTML.kt
@@ -2,7 +2,6 @@ package org.coramdeoacademy.view.html
 
 import org.coramdeoacademy.view.html.tags.TagWithText
 
-
 class HTML : TagWithText("html"){
     override fun render(builder: StringBuilder, indent: String) {
         builder.append("<!DOCTYPE html>\n")
