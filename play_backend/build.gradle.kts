f268f895437c94dd5f12c2708bb43bdf3c3a3ffb
Thomas Terry <darkforestsilence@gmail.com>
2021-08-06 14:48:19 -0500

    Update dependencies to use the new locations and remove extra package sources that aren't necessary anymore

--- play_backend/build.gradle.kts
+++ play_backend/build.gradle.kts
@@ -9,29 +9,12 @@ plugins {
 dependencies {
     implementation("commons-lang:commons-lang:20030203.000129")
     implementation("com.typesafe.play:play-guice_2.12:2.8.8")
-    implementation("com.github.casually-blue:cucm:1.4")
+    implementation("io.github.casually-blue:cucm:1.5")
     implementation("com.github.casually-blue:web:1.1")
 }
 
 repositories {
     mavenCentral()
-    maven {
-        name = "lightbend-maven-release"
-        url = uri("https://repo.lightbend.com/lightbend/maven-releases")
-    }
-    ivy {
-        name = "lightbend-ivy-release"
-        url = uri("https://repo.lightbend.com/lightbend/ivy-releases")
-        layout("ivy")
-    }
-    maven {
-        name = "gradle-js"
-        url = uri("https://repo.gradle.org/gradle/javascript-public")
-    }
-
-    maven {
-        url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
-    }
 
     maven {
         name = "Github Packages"
