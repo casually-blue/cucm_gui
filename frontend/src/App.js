ec7496c6fb3cd90ec14b1c34916873ae41a70145
Thomas Terry <darkforestsilence@gmail.com>
2021-08-16 17:42:23 -0500

    Rewrite react package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,27 +11,66 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        //fetch("http://localhost:9000/phones")
-        //    .then(res => res.json())
-        //    .then(
-        //        (result) => {
-        //            this.setState({
-        //                isLoaded: true,
-        //                phones: result
-        //            })
-        //        }
-        //    )
+        fetch("http://localhost:9000/phones")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        phones: result
+                    })
+                }
+            )
+    }
+
+    renderTemplate(val) {
+        return <div className="App">
+            <header className="App-Header">
+                <h1>Phones</h1>
+                {val}
+            </header>
+        </div>
     }
 
     render() {
+        if(this.state.isLoaded === false) {
+            return (
+                this.renderTemplate(this.renderUnloaded())
+            );
+        } else {
+            return (
+                this.renderTemplate(this.renderLoaded())
+            );
+        }
+    }
+
+    renderUnloaded() {
+        return <p>"Loading..."</p>;
+    }
+
+    renderLoaded() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1>CUCM Admin Page</h1>
-                    <div className='phones-container'>
-                    </div>
-                </header>
-            </div>
+            <table style={{border: "1px solid black", padding: "5px", marginLeft: "auto", marginRight:"auto"}}>
+                <thead>
+                <td>Index</td>
+                {Object.keys(this.state.phones[0]).slice(0,4).map(function(key) {
+                    return <td>{key.toUpperCase()}</td>
+                })}
+                </thead>
+                <tbody>
+                {this.state.phones.map(function(phone, index) {
+                    return <tr>
+                        <td>{index}</td>
+                        {
+
+                            Object.keys(phone).slice(0,4).map(function (key) {
+                                return <td>{(phone[key] || "none").toString()}</td>
+                            })
+                        }
+                    </tr>
+                })}
+                </tbody>
+            </table>
         );
     }
 }
